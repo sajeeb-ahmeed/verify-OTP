596bb500f75e94266193ccb431ec9a92ff865033
sajeeb-ahmeed <sajeeb.web@gmail.com>
2022-02-14 23:29:08 +0600

    comon function

---
+++ app.js
@@ -0,0 +1,10 @@
+function getPin() {
+    const pin = Math.round(Math.random() * 10000);
+    const pinString = pin + '';
+    if (pinString.length == 4) {
+        return pin;
+    } else {
+        // console.log('got 3 digit and calling again', pin);
+        return getPin();
+    }
+}
