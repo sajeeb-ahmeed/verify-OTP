04a55ca289483bc7ae2d69e6c0ae4c6a38b4e9fe
sajeeb-ahmeed <sajeeb.web@gmail.com>
2022-02-14 23:31:11 +0600

    input value

--- app.js
+++ app.js
@@ -14,3 +14,21 @@ function generatePin() {
     const pin = getPin();
     document.getElementById('display-pin').value = pin;
 }
+
+// calc input value
+document.getElementById('key-pad').addEventListener('click', function (event) {
+    const number = event.target.innerText;
+    const inputNumber = document.getElementById('typed-numbers');
+    if (isNaN(number)) {
+        if (number == 'C') {
+            inputNumber.value = '';
+        } else if (number == '<') {
+            inputNumber.value = parseInt(inputNumber.value / 10)
+        }
+    } else {
+        const calcNumber = inputNumber.value;
+        const newNumber = calcNumber + number;
+        inputNumber.value = newNumber;
+    }
+
+})
